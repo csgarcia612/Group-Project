577998a68c7ec4f79a9e99a58feee83643b811c6
Kyle Boysen <kyle.boysen@gmail.com>
2019-02-04 12:56:25 -0700

    add reducer, store, provider

---
+++ src/dux/reducer.js
@@ -0,0 +1,27 @@
+const INITIAL_STATE = {
+    user: null,
+}
+
+const SET_USER = 'SET_USER'
+
+function reducer(state = INITIAL_STATE, action){
+    switch(action.type){
+
+        case SET_USER:
+        return Object.assign({}, state, {user: action.payload})
+
+        default:
+        return state
+    }
+}
+
+
+export function setUser(user){
+    console.log('setUser in reducer', user);
+    return {
+        type: SET_USER,
+        payload: user
+    }
+}
+
+export default reducer;
