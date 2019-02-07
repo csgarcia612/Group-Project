63f79912e35368d69135eab4535ca49c5c34c148
csgarcia612 <csgarcia612@comcast.net>
2019-02-06 18:45:22 -0700

    single result component

--- src/components/home_page/home.js
+++ src/components/home_page/home.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './home.scss';
-import { connect } from 'react-redux';
-import { setEvents, setCity } from '../../dux/reducer';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import axios from "axios";
+import "./home.scss";
+import { connect } from "react-redux";
+import { setEvents, setCity } from "../../dux/reducer";
+import { NavLink } from "react-router-dom";
 
 class home extends Component {
 	constructor() {
@@ -50,15 +50,18 @@ class home extends Component {
 		});
 	};
 
-  searchEvents = (e) => {
-    let baseSearch = `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=eIMh2CGNhtUTSybN21TU3JRes1j9raV3&radius=50&sort=date,desc&classificationName=[music]&unit=miles`
-    let customSearch = baseSearch + '&city=' + e.city;
-    this.props.setCity(e.city);
-    axios.get(customSearch).then( response => {
-      console.log('response.data in home before setting redux state', response.data)
-      this.props.setEvents(response.data);
-    })
-  }
+	searchEvents = e => {
+		let baseSearch = `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=eIMh2CGNhtUTSybN21TU3JRes1j9raV3&radius=50&sort=date,desc&classificationName=[music]&unit=miles`;
+		let customSearch = baseSearch + "&city=" + e.city;
+		this.props.setCity(e.city);
+		axios.get(customSearch).then(response => {
+			console.log(
+				"response.data in home before setting redux state",
+				response.data
+			);
+			this.props.setEvents(response.data);
+		});
+	};
 
 	getHighlightedText = (text, highlight) => {
 		// Split text on higlight term, include term itself into parts, ignore case
@@ -80,7 +83,7 @@ class home extends Component {
 		const { filteredLocations, locations, searchQuery } = this.state;
 		const searchDropDown = filteredLocations.map(e => {
 			return (
-				<div id={e.rank} className="search-dropdown">
+				<div key={e.rank} className="search-dropdown">
 					<NavLink to="/search">
 						<p onClick={() => this.searchEvents(e)}>
 							{e.city}, {e.state}
@@ -109,7 +112,8 @@ class home extends Component {
 
 //   }
 // }
-let _
-export default connect(_, { setEvents, setCity })(home)
-
-
+let _;
+export default connect(
+	_,
+	{ setEvents, setCity }
+)(home);
--- src/components/search_results_page/search_results.js
+++ src/components/search_results_page/search_results.js
@@ -1,64 +1,62 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import "./search_results.scss";
+import SingleResult from "../single_search_result/single_result";
 
 class search_results extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            city: '&city=' + this.props.city,
-            distance: '&radius=',
-            genre: '&genre'
+	constructor(props) {
+		super(props);
+		this.state = {
+			city: "&city=" + this.props.city,
+			distance: "&radius=",
+			genre: "&genre"
+		};
+	}
 
-        }
-    }
+	handleUserInput = e => {
+		console.log(e.target.value);
+	};
 
-    handleUserInput = (e) => {
-        console.log(e.target.value)
-    }
-
-    render() {
-    const eventsList = this.props.events && this.props.events._embedded.events.map(e => {
-        return <p key={e.id}>{e.name}</p>
-    })
-    return (
-        <div className="search-results-container">
-            <div className="filters">
-                <input
-                    name='startDate' 
-                    type='date'
-                    onChange={e => this.handleUserInput(e)}
-                />
-                <input 
-                    name='endDate' 
-                    type='date' 
-                    onChange={e => this.handleUserInput(e)}
-                />
-                <input 
-                    type='number' 
-                    name='radius' 
-                    min='10' 
-                    max='100' 
-                    step='10' 
-                    placeholder='50' 
-                    onChange={e => this.handleUserInput(e)}
-                />
-                <select onChange={e => this.handleUserInput(e)} >
-                    <option value='genre1'>Pop</option>
-                    <option value='genre2'>Rock</option>
-                    <option value='genre3'>Jazz</option>
-                    <option value='genre4'>Blues</option>
-                </select>
-                <input 
-                    type='number' 
-                    onChange={e => this.handleUserInput(e)}
-                />
-            </div>
-            <div className="events-list">
-                {eventsList}
-            </div>
-        </div>
-    )
-  }
+	render() {
+		const eventsList =
+			this.props.events &&
+			this.props.events._embedded.events.map(e => {
+				return <SingleResult key={e.id} event={e} />;
+			});
+		return (
+			<div className="search-results-container">
+				<div className="filters">
+					<input
+						name="startDate"
+						type="date"
+						onChange={e => this.handleUserInput(e)}
+					/>
+					<input
+						name="endDate"
+						type="date"
+						onChange={e => this.handleUserInput(e)}
+					/>
+					<input
+						type="number"
+						name="radius"
+						min="10"
+						max="100"
+						step="10"
+						placeholder="50"
+						onChange={e => this.handleUserInput(e)}
+					/>
+					<select onChange={e => this.handleUserInput(e)}>
+						<option value="genre1">Pop</option>
+						<option value="genre2">Rock</option>
+						<option value="genre3">Jazz</option>
+						<option value="genre4">Blues</option>
+					</select>
+					<input type="number" onChange={e => this.handleUserInput(e)} />
+				</div>
+				<div className="events-list">{eventsList}</div>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = state => {
